b71b7cef236328241db00e95de01efb65ce60cf6
Dimitrije Jankovic <dimitrije_jankovic@outlook.com>
2020-10-05 17:15:23 +0200

    simple components

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
-import React from 'react';
-import { BrowserRouter } from 'react-router-dom';
+import React from 'react'
+import { BrowserRouter, Switch, Route } from 'react-router-dom'
+
+import Dashboard from './components/dashboard/Dashboard'
 
 // components
 import Navbar from './components/layout/Navbar'
+import ProjectDetails from './components/projects/ProjectDetails'
 
 function App() {
   return (
     <BrowserRouter>
     <div className="App">
       <Navbar />
+      <Switch>
+        <Route exact path="/" component={Dashboard}/>
+        <Route path='/project/:id' component={ProjectDetails} />
+      </Switch>
     </div>
     </BrowserRouter>
     
---
+++ src/components/projects/ProjectList.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import ProjectSummery from './ProjectSummery'
+
+const PojectList = () => {
+    return (
+        <div className="project-list section">
+            <ProjectSummery />
+            <ProjectSummery />
+            <ProjectSummery />
+            <ProjectSummery />
+        </div>
+    )
+}
+
+export default PojectList
