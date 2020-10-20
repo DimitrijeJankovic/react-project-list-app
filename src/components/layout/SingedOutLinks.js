b8cefc06b9d0c388968cbc5efd63e508f3cb2ff9
Dimitrije Jankovic <dimitrije_jankovic@outlook.com>
2020-10-20 22:25:29 +0200

    simple styles

--- src/components/layout/SingedOutLinks.js
+++ src/components/layout/SingedOutLinks.js
@@ -4,8 +4,8 @@ import { NavLink } from 'react-router-dom'
 const SingedOutLinks = () => {
     return(
         <ul className="right">
-            <li><NavLink to="/">Signup</NavLink></li>
-            <li><NavLink to="/">Login</NavLink></li>
+            <li><NavLink to="/singup">Signup</NavLink></li>
+            <li><NavLink to="/singin">Login</NavLink></li>
         </ul>
     )
 }
